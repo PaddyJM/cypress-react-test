d86ddecb389fb4c4a6e627509f6ad094872aee75
Patrick Morton <patrickjamesmorton@gmail.com>
2022-03-30 18:42:12 +0100

    removed deprecated cypress unit tests and migrated cypress intergration test to appropriate folder

--- src/components/ui-kit/Link.spec.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import { mount } from "@cypress/react";
-import Link from './Link'
-
-it('Link', () => {
-    mount(<Link className='Test Class' href='www.google.co.uk'>Test Link</Link>)
-    cy.get('a:contains(Test Link)').contains('Test Link')
-    cy.get('a:contains(Test Link)').should('have.attr', 'href')
-})
